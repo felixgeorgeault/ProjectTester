aad37f925d96ed0cb7ffb60a7e3bdf77e02f16c9
Felix Georgeault <fgeorgea@mi-c6-p4.s19.be>
2023-03-30 18:03:19 +0200

    all fixed

--- felixouxTester.sh
+++ felixouxTester.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+# ------------------------- CHECK REMAINING FILES --------------------------
 files=$(find . \( -name "a.out" -o -name "push_swap" -o -name "fdf" -o -name "pipex" -o -name "so_long" -o -name "minitalk" -o -name "fractol" -o -name "minishell" \) | wc -l);
 
 if [ $files -gt 0 ];
@@ -8,7 +9,9 @@ then
 	echo "KO -> found file that was not suppose to be here !";
 	exit 0;
 fi;
+# ------------------------- END CHECK REMAINING FILES -----------------------------
 
+# -------------------------- CHECK NORMINETTE -----------------------------------
 norm1=$(norminette | grep ": Error\!" | wc -l);
 
 norm2=$(norminette -R CheckDefine | grep ": Error\!" | wc -l);
@@ -20,13 +23,23 @@ then
 	echo "KO -> NORM ERROR !";
 	exit 0;
 fi;
+# ---------------------------- END CHECK NORMINETTE ------------------------------
 
+# ----------------- CHECK HEADER NAMES -------------------------------
 find . \( -name "*.c" -o -name "*.h" \) -exec grep "Created:" >> header_check.txt "{}" \;
 find . \( -name "*.c" -o -name "*.h" \) -exec grep "Updated:" >> header_check.txt "{}" \;
 
 file="header_check.txt";
 clear;
 is_header_correct=0;
+
+if [ $# != 0 ];
+then
+	OLD_USER=$USER;
+	USER=$1;
+fi;
+
+
 while read -r Line; do
 	if [[ "$Line" == *"$USER"* || "$Line" == *"Marvin42"* ]];
 	then
@@ -39,38 +52,40 @@ while read -r Line; do
 			is_header_correct=1;
 			echo "KO -> HEADER NAME MIGHT BE WRONG !";
 			echo $Line;
-			rm -rf header_check.txt
+			rm -rf header_check.txt;
+			USER=$OLD_USER;
 			exit 0;
 		fi;
 	fi;
 done < $file
 
 rm -rf header_check.txt
+USER=$OLD_USER;
 
 if [ $is_header_correct -gt 0 ];
 then
 	exit 0;
 fi;
-needs_francinette=$(find . \( -name "*libft*" -o -name "*get_next_line*" -o -name "*ft_printf*" \) | wc -l);
+# ----------------- END CHECK HEADER NAMES ------------------------------------
 
-if [ $needs_francinette -gt 0 ];
-then
-	
-	is_francinette=$(command -v francinette | wc -l);
-	if [ $is_francinette == 0 ];
-	then
-		bash -c "$(curl -fsSL https://raw.github.com/xicodomingues/francinette/master/bin/install.sh)";
-	else
-		/Users/$USER/francinette/tester.sh;
-	fi;
-fi;
 
-has_valgrind=$(command -v valgrind | wc -l);
+# ----------------------- CHECK FRANCINETTE -------------------------------
 
-if [ $has_valgrind == 0 ];
-then
-	brew install --HEAD LouisBrunner/valgrind/valgrind;
-fi;
+	#bash -c "$(curl -fsSL https://raw.github.com/xicodomingues/francinette/master/bin/install.sh)";
 
-echo "Looks good to me!";
+# ------------------------------ END CHECK FRANCINETTE ------------------------------
 
+
+# ----------------------------- CHECK BREW ------------------------------------------
+
+	#rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update
+
+# ------------------------------ END CHECK BREW ------------------------------------
+
+# ------------------------------ CHECK VALGRIND -----------------------------------
+
+	#brew install --HEAD LouisBrunner/valgrind/valgrind
+
+# ------------------------------- END CHECK VALGRIND -----------------------------------
+
+echo "OK !";
